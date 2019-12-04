7d0e0e3a8880573076f16e565e2fc71945a67256
Abdul Rafay Khan <rafaykhan.work@gmail.com>
2019-12-04 15:10:32 +0500

    work 2

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Work
+          Learn React Work 2
         </a>
       </header>
     </div>
